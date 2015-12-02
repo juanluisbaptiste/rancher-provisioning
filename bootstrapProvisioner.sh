7b53f5c61035b8c397aa4b79e7eaaa4c7372713c
James Kirk <james.kirk84@gmail.com>
2015-12-02 17:04:57 +0000

    ansible installing docker on hosts

---
+++ bootstrapProvisioner.sh
@@ -0,0 +1,16 @@
+apt-get update -y
+apt-get install -y software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update -y
+apt-get install -y ansible sshpass
+
+cp /vagrant/provisioning/inventory/vagrant_inventory /var/tmp
+chmod -x /var/tmp/vagrant_inventory
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+#install any ansible roles we may need
+ansible-galaxy install --force ateoto.docker
+
+# Now run the rancher provisioning
+ansible-playbook -i /var/tmp/vagrant_inventory /vagrant/provisioning/rancher-server.yml
+ansible-playbook -i /var/tmp/vagrant_inventory /vagrant/provisioning/rancher-agent.yml
