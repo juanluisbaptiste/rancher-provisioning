0851a142b0578686056294d382c1953e6e0d1516
James Kirk <james.kirk84@gmail.com>
2015-12-04 13:52:55 +0000

    refactoring separate scripts for server and agent

--- bootstrapProvisioner.sh
+++ bootstrapProvisioner.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+apt-add-repository -y ppa:ansible/ansible
+apt-get update -y
+apt-get install -y software-properties-common ansible sshpass
+
+cd /vagrant
+
 export RANCHER_SERVER_HOST=172.19.8.100
-export RANCHER_AGENT_HOST=172.19.8.101
+export RANCHER_PORT=8080
 export SSH_PASS=vagrant
 export SSH_USER=vagrant
 
-# Main provisioning entry point
-cd /vagrant/ && ./provision.sh
+./provisionServer.sh 
+
+export RANCHER_AGENT_NAME=rancher-agent-1
+export RANCHER_AGENT_HOST=172.19.8.101
+
+./provisionAgent.sh
---
+++ provisionServer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required variable checks
+SSH_USER=${SSH_USER?"Need to provide SSH_USER variable"}
+SSH_PASS=${SSH_PASS?"Need to provide SSH_PASS variable"}
+RANCHER_SERVER_HOST=${RANCHER_SERVER_HOST?"Need to provide RANCHER_SERVER_HOST variable"}
+RANCHER_PORT=${RANCHER_PORT?"Need to provide RANCHER_PORT variable"}
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+#install any ansible roles we may need
+ansible-galaxy install --force ateoto.docker
+
+ansible-playbook -u $SSH_USER -i $RANCHER_SERVER_HOST, --extra-vars "ansible_ssh_pass=$SSH_PASS rancher_port=$RANCHER_PORT" provisioning/rancher-server.yml
