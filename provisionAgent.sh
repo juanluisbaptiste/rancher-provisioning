0b22722c6ebf4a6220b03639d74d64f5b3aa03a0
James Kirk <james.kirk84@gmail.com>
2015-12-08 17:37:19 +0000

    fighting basic auth and losing

--- provisionAgent.sh
+++ provisionAgent.sh
@@ -5,6 +5,8 @@ SSH_USER=${SSH_USER?"Need to provide SSH_USER variable"}
 SSH_PASS=${SSH_PASS?"Need to provide SSH_PASS variable"}
 RANCHER_SERVER_HOST=${RANCHER_SERVER_HOST?"Need to provide RANCHER_SERVER_HOST variable"}
 RANCHER_PORT=${RANCHER_PORT?"Need to provide RANCHER_PORT variable"}
+ADMIN_USER=${ADMIN_USER?"Need to provide ADMIN_USER variable"}
+ADMIN_PASS=${ADMIN_PASS?"Need to provide ADMIN_PASS variable"}
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
@@ -14,5 +16,5 @@ ansible-galaxy install --force ateoto.docker
 ansible-playbook \
 	-u $SSH_USER \
 	-i $RANCHER_AGENT_HOST, \
-	--extra-vars "ansible_ssh_pass=$SSH_PASS RANCHER_SERVER=$RANCHER_SERVER_HOST RANCHER_PORT=$RANCHER_PORT" \
+	--extra-vars "ansible_ssh_pass=$SSH_PASS RANCHER_SERVER=$RANCHER_SERVER_HOST RANCHER_PORT=$RANCHER_PORT ADMIN_USER=$ADMIN_USER ADMIN_PASS=$ADMIN_PASS" \
 	provisioning/rancher-agent.yml
